0b9086568a144834c047430838e02049f2885bc9
Ali Nasra <15128@stemegypt.edu.eg>
2024-04-21 13:28:41 +0300

    File Migration
    Migrating the files of the project to the repository.

---
+++ python/helper.py
@@ -0,0 +1,164 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def calculate_distance(first_point,second_point):
+	dist_sqr = ((first_point[0]-second_point[0])**2)+((first_point[1]-second_point[1])**2)
+	return math.sqrt(dist_sqr)
+
+def calculate_yaw(first_point,second_point):
+	angle = math.atan2(second_point[1]-first_point[1],second_point[0]-first_point[0])
+	return angle
+
+def calculate_circular_trajectory(pose,v,w,aa,ta,time_step,time_window):
+	trajectory = np.array([[0,0,0,0,0]])
+	x          = pose[0]
+	y          = pose[1]
+	theta      = pose[2]
+	time_count = 0
+	while time_count < time_window:
+		theta = theta + (w * time_step)                                # update theta according to the kinematics equations
+		x = x + v * math.cos(theta) * time_step                        # Calculate the new x position
+		y = y + v * math.sin(theta) * time_step                        # Calculate the new y position
+		trajectory = np.vstack((trajectory, [x,y,theta,v,w])) 		   # Append pose info into the trajectory list	
+		time_count = time_count + time_step                            # Increment the time counter until it reaches the time window limit
+	return trajectory[1:]
+
+
+def filter_obstacles(pose,obstacles):
+	filtered_list = []
+	radius        = 2
+	for obstacle in obstacles:
+		if calculate_distance(pose,obstacle) < radius:
+			filtered_list.append(obstacle)
+	return np.array(filtered_list)
+
+
+def calculate_velocity_range(max_v,min_v,max_w,min_w,v,w,aa,ta,time_window):
+	range_vel = []
+	v_upper   = v + ta * time_window
+	v_lower   = v - ta * time_window
+	w_upper   = w + aa * time_window
+	w_lower   = w - aa * time_window
+	range_vel.append([max(v_lower,min_v),min(v_upper,max_v)])
+	range_vel.append([max(w_lower,min_w),min(w_upper,max_w)])
+	return range_vel
+
+
+def calculate_obstacle_cost(coefficient,trajectory,obstacles,vehicle_width,vehicle_height):
+	ox = obstacles[:, 0]
+	oy = obstacles[:, 1]
+	dx = trajectory[:, 0] - ox[:, None]
+	dy = trajectory[:, 1] - oy[:, None]
+	r = np.hypot(dx, dy)
+	yaw = trajectory[:, 2]
+	rot = np.array([[np.cos(yaw), -np.sin(yaw)], [np.sin(yaw), np.cos(yaw)]])
+	rot = np.transpose(rot, [2, 0, 1])
+	local_ob = obstacles[:, None] - trajectory[:, 0:2]
+	local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+	local_ob = np.array([local_ob @ x for x in rot])
+	local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+	upper_check = local_ob[:, 0] <= vehicle_height / 2
+	right_check = local_ob[:, 1] <= vehicle_width / 2
+	bottom_check = local_ob[:, 0] >= -vehicle_height / 2
+	left_check = local_ob[:, 1] >= -vehicle_width / 2
+	if (np.logical_and(np.logical_and(upper_check, right_check),
+                           np.logical_and(bottom_check, left_check))).any():
+		return float("Inf")
+	min_r = np.min(r)
+	return coefficient / min_r  # OK
+
+
+def calculate_goal_cost(coefficient,trajectory,goal):
+	dx = goal[0] - trajectory[-1, 0]
+	dy = goal[1] - trajectory[-1, 1]
+	error_angle = math.atan2(dy, dx)
+	cost_angle = error_angle - trajectory[-1, 2]
+	cost = coefficient * abs(math.atan2(math.sin(cost_angle), math.cos(cost_angle)))
+	return cost
+
+
+def calculate_velocity_cost(coefficient, trajectory,max_v):
+	cost = coefficient * (max_v - trajectory[0,3])
+	return cost
+
+
+def pick_trajectory(point,previous_point,goal,obstacles,v,w,max_v,max_w,min_v,min_w,gc,vc,oc,ta,aa,time_window,time_step,rv,rw,vehicle_width,vehicle_height):
+	yaw            = calculate_yaw(previous_point,point)
+	vel_range      = calculate_velocity_range(max_v,min_v,max_w,min_w,v,w,aa,ta,time_window)
+	#print("vel_range:",vel_range)
+	min_cost       = float("inf")
+	pose           = np.array([point[0],point[1],yaw])
+	vel_counter    = vel_range[0][0]
+	rot_counter    = vel_range[1][0]
+	resolution_v   = (vel_range[0][1]-vel_range[0][0])/rv
+	resolution_w   = (vel_range[1][1]-vel_range[1][0])/rw
+	ideal_traj     = []
+	obstacles_list = filter_obstacles(point,obstacles)
+	while vel_counter<=vel_range[0][1]:
+		while rot_counter<=vel_range[1][1]:
+			trajectory = calculate_circular_trajectory(pose,vel_counter,rot_counter,aa,ta,time_step,time_window)
+			if len(trajectory) == 0:
+				rot_counter = rot_counter + rw
+				continue
+			cost_1     = calculate_goal_cost(gc,trajectory,goal)  
+			cost_2	   = calculate_velocity_cost(vc,trajectory,max_v)
+			cost_3     = 0
+			if len(obstacles_list)>0:
+				cost_3     = calculate_obstacle_cost(oc,trajectory,obstacles_list,vehicle_width,vehicle_height)
+			cost_total = cost_1 + cost_2 + cost_3
+			#print("Angle:",trajectory[0,2],"Cost:",cost_total,"Coordinates: (",point[0],",",point[1],")")
+			if (cost_total<min_cost):
+				ideal_traj = np.array(trajectory)
+				min_cost   = cost_total
+			rot_counter = rot_counter + resolution_w
+		rot_counter = vel_range[1][0]
+		vel_counter = vel_counter + resolution_v
+	#plt.scatter(ideal_traj[:,0],ideal_traj[:,1])
+	#plt.show()
+	return ideal_traj,(ideal_traj[0,2])*2,(ideal_traj[0,3])
+
+
+
+def get_obstacle_points(m):
+	obstacle_x  = []
+	obstacle_y  = []
+	obstacles_no = len(m.geom_pos)-12
+	for i in range(1,obstacles_no,1):		
+		pos_x             = m.geom_pos[i][0]
+		pos_y             = m.geom_pos[i][1]
+		size_x            = m.geom_size[i][0]
+		size_y            = m.geom_size[i][1]
+		increment_x       = 0
+		increment_y       = 0
+		while increment_x <= size_x:
+				#Add 2 points on the right
+				obstacle_x.append(pos_x+increment_x)
+				obstacle_x.append(pos_x+increment_x)
+				obstacle_y.append(pos_y+size_y)
+				obstacle_y.append(pos_y-size_y)
+				#Add 2 points on the left
+				obstacle_x.append(pos_x-increment_x)
+				obstacle_x.append(pos_x-increment_x)
+				obstacle_y.append(pos_y+size_y)
+				obstacle_y.append(pos_y-size_y)
+				increment_x = increment_x + 0.1	
+
+		while increment_y <= size_y:
+				#Add 2 points on the left
+				obstacle_x.append(pos_x+size_x)
+				obstacle_x.append(pos_x+size_x)
+				obstacle_y.append(pos_y+increment_y)
+				obstacle_y.append(pos_y-increment_y)
+				#Add 2 points on the right
+				obstacle_x.append(pos_x-size_x)
+				obstacle_x.append(pos_x-size_x)
+				obstacle_y.append(pos_y+increment_y)
+				obstacle_y.append(pos_y-increment_y)
+				increment_y = increment_y + 0.1
+	#plt.scatter(obstacle_x,obstacle_y)
+	#plt.show()
+	return obstacle_x.copy(),obstacle_y.copy()
+	
+
---
+++ python/main.py
@@ -0,0 +1,124 @@
+import time
+import matplotlib.pyplot as plt
+import mujoco
+import mujoco.viewer
+import cmpe434_utils
+import numpy as np
+from helper import *
+from helper_2 import *
+import random
+import math
+"""
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "-3 6 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "4 0 0.05" density = "5000"/>
+    <geom type="box" size="0.1 2.6 0.1" rgba="0.01 .3 1 1" pos= "4  2 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "0.8 2 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "2 2 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "0 3 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "3 1 0.05" density = "5000"/>
+    <geom type="box" size="0.2 0.1 0.1" rgba="0.01 .3 1 1" pos= "2 0 0.05" density = "5000"/>
+"""
+scene, scene_assets = cmpe434_utils.get_model('scenes/racing_circuit.xml')
+robot, robot_assets = cmpe434_utils.get_model('models/mushr_car/model.xml')
+# robot, robot_assets = cmpe434_utils.get_model('models/mujoco_car/model.xml')
+# robot, robot_assets = cmpe434_utils.get_model('models/skydio_x2/x2.xml')
+
+# Add the robot to the scene.
+scene.include_copy(robot)
+
+# Combine all assets into a single dictionary.
+all_assets = {**scene_assets, **robot_assets}
+
+m = mujoco.MjModel.from_xml_string(scene.to_xml_string(), assets=all_assets)
+d = mujoco.MjData(m) 
+
+
+
+paused = False # Global variable to control the pause state.
+
+obstaclesx,obstaclesy = get_obstacle_points(m)
+obstacles = np.column_stack([obstaclesx, obstaclesy])
+# Pressing SPACE key toggles the paused state. 
+# You can define other keys for other actions here.
+def key_callback(keycode):
+  if chr(keycode) == ' ':
+    global paused
+    paused = not paused
+
+
+
+with mujoco.viewer.launch_passive(m, d, key_callback=key_callback) as viewer:
+
+  # velocity = m.actuator("throttle_velocity")
+  # steering = m.actuator("steering")
+
+  velocity       = d.actuator("throttle_velocity")
+  steering       = d.actuator("steering")
+  start          = time.time()
+  prev_point     = np.array([d.xpos[3][0:2],d.xpos[4][0:2],d.xpos[5][0:2],d.xpos[6][0:2]])
+  prev_point     = np.mean(prev_point, axis=0)
+  max_v          = 5
+  max_w          = 50.0 * math.pi / 180.0
+  min_v          = -2.5
+  min_w          = -1*max_w
+  gc             = 0.15
+  vc             = 1
+  oc             = 1
+  ta             = 2
+  aa             = 50.0 * math.pi / 180.0
+  time_window    = 1
+  time_step      = 0.1
+  rv             = 4
+  rw             = 4
+  vehicle_width  = 0.25
+  vehicle_height = 0.2965
+  #tile_count     = 0
+  while viewer.is_running() and time.time() - start < 10000:
+    step_start = time.time()
+
+    if not paused:
+        #list(viewer.user_scn.geoms).clear()
+        #viewer.user_scn.ngeom   = 0
+        #tile_count              = 0
+        #print(d.xpos)
+        point        = np.array([d.xpos[3][0:2],d.xpos[4][0:2],d.xpos[5][0:2],d.xpos[6][0:2]])
+        point        = np.mean(point, axis=0)
+        #print(point)
+        velocity_val = np.mean(np.array(d.sensordata[0:3]))
+        angular_val  = np.mean(np.array(d.sensordata[3:6]))
+        #velocity_val = math.sqrt((v_measure[0]**2)+(v_measure[1]**2))
+        #angular_val  = math.sqrt((w_measure[0]**2)+(w_measure[1]**2))
+        goal         = np.array([-3,7])
+        #goal         = [random.uniform(-6,4),random.uniform(-2,8)]
+        traj,s,v     = pick_trajectory(point,prev_point,goal,obstacles,velocity_val,angular_val,max_v,max_w,min_v,min_w,gc,vc,oc,ta,aa,time_window,time_step,rv,rw,vehicle_width,vehicle_height)
+        #print("Goal:",goal)
+        #print("Steering:",s," - ","Velocity:",v)
+        """
+        for point in traj:
+            mujoco.mjv_initGeom(
+                      viewer.user_scn.geoms[tile_count],
+                      type=mujoco.mjtGeom.mjGEOM_CYLINDER,
+                      size = [0.05, 0.,0.],
+                      pos =np.array([point[0], point[1], 0.05]),
+                      mat =np.eye(3).flatten(),
+                      rgba=np.array([0.89, 0.051, 0.051,0.4])
+              )            
+            tile_count += 1
+        viewer.user_scn.ngeom = tile_count
+        """
+        #control_vals  = DWA(12,12,obstacles)
+        #steering.ctrl = control_vals[0]
+        #velocity.ctrl = control_vals[1]
+        #print(s,v)
+        steering.ctrl  =  s
+        velocity.ctrl  =  v
+        prev_point     = point.copy()
+        mujoco.mj_step(m, d)
+        # Pick up changes to the physics state, apply perturbations, update options from GUI.
+        viewer.sync()
+    else:
+        break
+    # Rudimentary time keeping, will drift relative to wall clock.
+    time_until_next_step = m.opt.timestep - (time.time() - step_start)
+    if time_until_next_step > 0:
+      time.sleep(time_until_next_step)
